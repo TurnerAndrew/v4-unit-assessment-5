7e4a5f1aa7c6116c88a0612f7bbde295ed105107
Andrew Turner <turner.andrew0@gmail.com>
2020-12-15 10:45:47 -0600

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,12 +1,39 @@
 require('dotenv').config();
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 const express = require('express'),
       userCtrl = require('./controllers/user'),
       postCtrl = require('./controllers/posts')
+const massive = require('massive')
+const session = require('express-session')
 
 
 const app = express();
 
 app.use(express.json());
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
+
+massive ({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then((dbInstance => {
+    app.set('db', dbInstance)
+    console.log('DB Ready')
+    app.listen(SERVER_PORT, () => 
+    console.log(`running on ${SERVER_PORT}`));
+    
+}))
+
+
+
 
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
@@ -19,5 +46,3 @@ app.get('/api/posts', postCtrl.readPosts);
 app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
-
-app.listen(4000, _ => console.log(`running on ${4000}`));
