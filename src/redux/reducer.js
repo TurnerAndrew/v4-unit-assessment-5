192450e937658adc4d9b9ef94ad2daf556102838
Andrew Turner <turner.andrew0@gmail.com>
2020-12-15 18:01:17 -0600

    submission

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,33 +1,32 @@
 const initialState = {
-    username: '',
-    profilePic: ''
-}
+  username: "",
+  profilePic: "",
+};
 
-const UPDATE_USER = 'UPDATE_USER'
-const LOGOUT = 'LOGOUT'
+const UPDATE_USER = "UPDATE_USER";
+const LOGOUT = "LOGOUT";
 
 export const updateUser = (user) => {
-    return {
-        type: UPDATE_USER,
-        payload: user
-    }
-}
+  return {
+    type: UPDATE_USER,
+    payload: user,
+  };
+};
 
 export const logout = () => {
-    return {
-        type: LOGOUT,
-    }
-}
+  return {
+    type: LOGOUT,
+  };
+};
 
-
-export default function reducer (state = initialState, action) {
-    switch (action.type) {
-        case UPDATE_USER :
-            const {username, profilePic} = action.payload.user
-            return {username, profilePic}
-        case LOGOUT:
-            return state = initialState
-        default: 
-            return state
-    }
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_USER:
+      const { username, profilePic } = action.payload;
+      return { username, profilePic };
+    case LOGOUT:
+      return (state = initialState);
+    default:
+      return state;
+  }
 }
